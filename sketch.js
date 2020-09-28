bb224606dd95389255c689ebee74a9815abf8de8
srinivas <srinivas8279@gmail.com>
2020-09-28 18:00:44 +0530

    changed background

--- sketch.js
+++ sketch.js
@@ -7,11 +7,12 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var bg="sprites/bg.png";
 var gameState = "onSling";
+var score=0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getbackgroundimg();
 }
 
 function setup(){
@@ -45,18 +46,26 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+    noStroke()
+    textSize(30)
+    fill("white")
+    text("score:"+score,width-200,50)
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +94,19 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getbackgroundimg(){
+    var responce=await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    
+    var responceJSON= await responce.json()
+    console.log(responceJSON);
+    var time= responceJSON.datetime.slice(11,13)
+    console.log(time);
+    if(time>6 && time<18){
+        bg="sprites/bg.png"
+    }else{
+        bg="sprites/bg2.jpg"
+    }
+    backgroundImg=loadImage(bg)
 }
